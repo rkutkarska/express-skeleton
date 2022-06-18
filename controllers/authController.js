e879d9f2ce35864cfe13f701d6f1e6ee216b7b74
rkutkarska <ralena.dimitrova@gmail.com>
2022-06-18 21:43:42 +0300

    Add user model and register validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
@@ -8,9 +9,23 @@ router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.end();
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.render('auth/register', { error: 'Password missmatch!' });
+    }
+
+    try {
+        // Create user
+        const createdUser = await authService.create({ username, password });
+        res.redirect('/login');
+
+    } catch (error) {
+        // Add mongoose error mapper
+        return res.render('auth/register', { error: 'db error' });
+    }
+
 });
 
 module.exports = router;
