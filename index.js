f65ab5aec55924f6861ec5841f5c25418099bb40
rkutkarska <ralena.dimitrova@gmail.com>
2022-06-18 14:25:52 +0300

    Config view engine and add routes

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const { PORT } = require('./config/env');
+const routes = require('./routes');
+
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine = ('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
+app.use(routes);
+
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}...`));
